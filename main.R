c7218629c6af7a2059ccbaa6e2525ed7fa047006
Yigit Ali Demir <yigitalidemir@Yigits-MacBook-Air.local>
2024-10-01 17:54:50 +0200

    first code

--- main.R
+++ main.R
@@ -1,1 +1,14 @@
-bike <- read.csv("SeoulBikeData.csv", check.names = FALSE)
+
+data <- read.csv("SeoulBikeData.csv", fileEncoding = "CP949")
+library(ggplot2)
+head(data)
+str(data)
+View(data)
+data$Seasons <- as.factor(data$Seasons)
+data$Holiday <- as.factor(data$Holiday)
+colnames(data)[colnames(data) == "Temperature.캜."] <- "Temperature"
+colnames(data)[colnames(data) == "Humidity..."] <- "Humidity"
+colnames(data)[colnames(data) == "Dew.point.temperature.캜."] <- "Dew.point.temperature"
+data <- data[data$Functioning.Day != "No", ]
+View(data)
+str(data)
